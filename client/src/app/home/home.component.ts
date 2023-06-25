48337231d395358dd4af6cd1a7a3ad71946cdb8c
tws9797 <wensengtee@gmail.com>
2023-06-25 20:44:11 +0800

    routing in Angular

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  users: any;
+
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: resp => this.users = resp,
+      error: err => console.log(err),
+      complete: () => console.log('Request has completed'),
+    })
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
+}
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import {Observable, of} from "rxjs";
+import { User } from '../_models/user';
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-nav',
@@ -8,24 +12,21 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn = false;
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {}
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        this.loggedIn = true;
-      },
-      error: error => console.log(error)
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error => this.toastr.error(error.error)
     })
   }
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
