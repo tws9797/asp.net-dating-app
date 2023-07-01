a82ee2b3dc73a564936062cffa605306117962ce
tws9797 <wensengtee@gmail.com>
2023-07-01 19:49:38 +0800

    Building the user interface

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
@@ -10,24 +9,14 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   users: any;
 
-  constructor(private http:HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getUsers();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: resp => this.users = resp,
-      error: err => console.log(err),
-      complete: () => console.log('Request has completed'),
-    })
-  }
-
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
   }
